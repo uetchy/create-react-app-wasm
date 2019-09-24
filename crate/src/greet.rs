8eca05e4158d50eebafa8b18046eee665a603af1
Yasuaki Uechi <y@uechi.io>
2019-09-24 22:26:53 +0900

    docs: add comments

--- crate/src/greet.rs
+++ crate/src/greet.rs
@@ -1,19 +1,19 @@
-extern crate serde;
-extern crate serde_json;
-use std::panic;
 use wasm_bindgen::prelude::*;
 use web_sys;
 
 #[wasm_bindgen]
 extern "C" {
+  // map [rust] fn alert(s: &str) => [ts] global.alert(s: string)
   fn alert(s: &str);
 
+  // map [rust] jsLog(s: &str) => [ts] console.log(s: string)
   #[wasm_bindgen(js_namespace = console, js_name = log)]
   fn jsLog(s: &str);
 }
 
 #[wasm_bindgen(module = "/../web/src/utils/text.ts")]
 extern "C" {
+  // map [rust] modLog(s: &str) => [ts] modLog(s: string) in @web/src/utils/text.rs
   fn modLog(s: &str);
 }
 
@@ -21,10 +21,13 @@ macro_rules! console_log {
   ($($t:tt)*) => (jsLog(&format_args!($($t)*).to_string()))
 }
 
+// map [ts] greet(s: string, array: string[]): string => [rust] green(s: &str, array: JsValue) -> String
 #[wasm_bindgen]
 pub fn greet(s: &str, array: JsValue) -> String {
+  // map [ts] string[] => [rust] Vec<String>
+  web_sys::console::log_1(&"👍 web_sys::console::log_1".into());
   let elements: Vec<String> = array.into_serde().unwrap();
-  web_sys::console::log_1(&format!("{} -> {}", "👍 web_sys::console::log_1", s).into());
+  modLog(s);
   for i in elements.iter() {
     modLog(i);
   }
